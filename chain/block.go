dad886df6ae476d1e4aafc15b966001f829e727f
Patrick O'Grady <prohb125@gmail.com>
2022-03-31 15:26:41 -0700

    fix logging

--- chain/block.go
+++ chain/block.go
@@ -136,7 +136,7 @@ func (b *StatelessBlock) ID() ids.ID { return b.id }
 func generateRandom(db database.Database, pid ids.ID, hght uint64) (common.Hash, error) {
 	v := SelectRandomValueKey(db, hght)
 	if v == (common.Hash{}) {
-		log.Debug("no key found for random", "parent", hexutil.Encode(pid[:]), "val", v, "index", hght)
+		log.Debug("no key found for random", "parent", hexutil.Encode(pid[:]), "height", hght)
 		return common.Hash{}, nil
 	}
 
@@ -149,7 +149,7 @@ func generateRandom(db database.Database, pid ids.ID, hght uint64) (common.Hash,
 	}
 
 	rand := ValueHash(append(val, pid[:]...))
-	log.Info("generated random", "random", rand, "parent", hexutil.Encode(pid[:]), "key", v)
+	log.Debug("generated random", "random", rand, "parent", hexutil.Encode(pid[:]), "key", v)
 	return rand, nil
 }
 
